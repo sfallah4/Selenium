cdd60b23443f364e1ab4d3939e915639d09dbdff
sfallah4 <sofia.fallah@iths.se>
2022-11-22 20:08:11 +0100

    when empty contacts in the contactlist is consistent

--- contacts.py
+++ contacts.py
@@ -7,3 +7,11 @@ class Contact:
         self.numbers = {}
     def lookup(self, name):
         return self.numbers[name]
+    def is_consistent(self):
+        for name1, number1 in self.numbers.items():
+            for name2, number2 in self.numbers.items():
+                if name1 == name2:
+                    continue
+                if number1.startswith(number2):
+                    return False
+        return True
