67298f07768a1ddc7e5aa55164886935f99f3b34
sfallah4 <sofia.fallah@iths.se>
2022-11-30 18:16:37 +0100

    Opening a webpage with webDriver

--- Ikea.py
+++ Ikea.py
@@ -1,4 +1,5 @@
 from selenium import webdriver
+from selenium.webdriver.common.by import By
 from selenium.webdriver.common.keys import Keys
 from selenium.webdriver.chrome.options import Options
 import time
@@ -6,14 +7,17 @@ import time
 chromeOptions = Options()
 chromeOptions.add_argument("--kiosk")
 
-driver = webdriver.Chrome(options=chromeOptions)
+driver=webdriver.Chrome(options=chromeOptions)
 driver.get("https://www.ikea.com/se/sv/")
 
-
-ele = driver.find_element_by_name("q")
+ele = driver.find_element(By.NAME, "q")
 time.sleep(2)
 ele.clear()
-ele.send_keys("stoense")
+ele.send_keys("matta")
 ele.send_keys(Keys.RETURN)
+
+ele_link = driver.find_element(By.LINK_TEXT, "stoense")
+time.sleep(2)
+ele_link.click()
 
 driver.close()
