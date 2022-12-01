e50c88f5286867af077acb0c6f20b4d6eece9832
sfallah4 <sofia.fallah@iths.se>
2022-12-01 01:17:15 +0100

    Opening a webpage with webDriver

--- Ikea.py
+++ Ikea.py
@@ -30,19 +30,19 @@ conf_email.send_keys("sofia.fallah@iths.se")
 new_pass = driver.find_element(By.NAME, "reg_passwd__")
 new_pass.clear()
 new_pass.send_keys("Sofia6738!")
-time.sleep(2)
+time.sleep(1)
 day_element = driver.find_element(By.XPATH, "//select[@aria-label='Dag']")
 all_options = day_element.find_elements(By.TAG_NAME, "option")
 for option in all_options:
     if option.get_attribute("value") == "4":
         option.click()
-time.sleep(2)
+time.sleep(1)
 month_element = driver.find_element(By.XPATH, "//select[@aria-label='Månad']")
 all_options = month_element.find_elements(By.TAG_NAME, "option")
 for option in all_options:
     if option.get_attribute("value") == "nov":
         option.click()
-time.sleep(2)
+time.sleep(1)
 year_element = driver.find_element(By.XPATH, "//select[@aria-label='År']")
 all_options = year_element.find_elements(By.TAG_NAME, "option")
 for option in all_options:
@@ -52,9 +52,9 @@ time.sleep(1)
 checkbox = driver.find_element(By.XPATH, "//label[@class='_58mt']")
 checkbox.click()
 
-submit_ele = driver.find_element(By.XPATH, "//button[@type='submit']")
-time.sleep(2)
-submit_ele.click()
+submit_ele = driver.find_element(By.XPATH, "//button[@type='submit']").click();
+#time.sleep(1)
+#submit_ele.click()
 
 #name = driver.find_element(By.NAME, "q")
 #time.sleep(2)
