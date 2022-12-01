54a81e050c388d09d4bc60bf2cfa76a0ca4310e2
sfallah4 <sofia.fallah@iths.se>
2022-12-01 20:18:10 +0100

    Opening a webpage with webDriver. adding time.sleep

--- Ikea.py
+++ Ikea.py
@@ -13,20 +13,25 @@ driver=webdriver.Chrome(options=chromeOptions)
 driver.get("https://sv-se.facebook.com/r.php?locale=sv_SE&display=page")
 
 ele = WebDriverWait(driver, 10).until(
-    EC.element_to_be_clickable((By.NAME, "firstname")))
+    EC.element_to_be_clickable((By.XPATH, '//button[@data-testid="cookie-policy-manage-dialog-accept-button"]'))).click();
+time.sleep(5)
 
 first_name = driver.find_element(By.NAME, "firstname")
 first_name.clear()
 first_name.send_keys("Sofia")
+time.sleep(1)
 last_name = driver.find_element(By.NAME, "lastname")
 last_name.clear()
 last_name.send_keys("Fallah")
+time.sleep(1)
 email = driver.find_element(By.NAME, "reg_email__")
 email.clear()
 email.send_keys("sofia.fallah@iths.se")
+time.sleep(1)
 conf_email = driver.find_element(By.NAME, "reg_email_confirmation__")
 conf_email.clear()
 conf_email.send_keys("sofia.fallah@iths.se")
+time.sleep(1)
 new_pass = driver.find_element(By.NAME, "reg_passwd__")
 new_pass.clear()
 new_pass.send_keys("Sofia6738!")
@@ -53,7 +58,7 @@ checkbox = driver.find_element(By.XPATH, "//label[@class='_58mt']")
 checkbox.click()
 
 submit_ele = driver.find_element(By.XPATH, "//button[@type='submit']").click();
-#time.sleep(1)
+time.sleep(10)
 #submit_ele.click()
 
 #name = driver.find_element(By.NAME, "q")
