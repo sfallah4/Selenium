41b4efa3547ccf03b74860b05c1ea758f5d7dc55
sfallah4 <sofia.fallah@iths.se>
2022-12-13 22:13:59 +0100

    add asserts

--- facebook.py
+++ facebook.py
@@ -4,7 +4,7 @@ from selenium.webdriver.common.by import By
 from selenium.webdriver.chrome.options import Options
 from selenium.webdriver.support.ui import WebDriverWait
 from selenium.webdriver.support import expected_conditions as EC
-from logins import Login
+from facebook_test import Login
 import time
 
 class LoginTest(unittest.TestCase):
