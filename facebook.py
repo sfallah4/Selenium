68bc34999b0b07dfd5fc8f90adc7e87f7c08114c
sfallah4 <sofia.fallah@iths.se>
2022-12-14 01:08:02 +0100

    adding more asserts

--- facebook.py
+++ facebook.py
@@ -7,9 +7,12 @@ from selenium.webdriver.support import expected_conditions as EC
 from facebook_test import Login
 import time
 
+
 class LoginTest(unittest.TestCase):
     def setUp(self):
         self.logins = Login()
+
+
 chromeOptions = Options()
 chromeOptions.add_argument("--kiosk")
 
@@ -17,47 +20,63 @@ driver = webdriver.Chrome(options=chromeOptions)
 driver.get("https://sv-se.facebook.com/r.php?locale=sv_SE&display=page")
 
 ele = WebDriverWait(driver, 10).until(
-    EC.element_to_be_clickable((By.XPATH, '//button[@data-testid="cookie-policy-manage-dialog-accept-button"]'))).click();
+ EC.element_to_be_clickable((By.XPATH, '//button[@data-testid="cookie-policy-manage-dialog-accept-button"]'))).click();
 time.sleep(1)
 
 first_name = driver.find_element(By.NAME, "firstname")
 first_name.clear()
 first_name.send_keys("Sofia")
+
+
 def test_lookup_by_firstname(self):
     self.logins.add("Sofia", "sofia.fallah@iths.se")
     login = self.logins.lookup("Sofia")
     self.assertEqual("sofia.fallah@iths.se", login)
+
+
 time.sleep(1)
 last_name = driver.find_element(By.NAME, "lastname")
 last_name.clear()
 last_name.send_keys("Fallah")
+
+
 def test_lookup_by_lastname(self):
     self.logins.add("Fallah", "sofia.fallah@iths.se")
     login = self.logins.lookup("Fallah")
     self.assertEqual("sofia.fallah@iths.se", login)
+
+
 time.sleep(1)
 email = driver.find_element(By.NAME, "reg_email__")
 email.clear()
 email.send_keys("sofia.fallah@iths.se")
+
+
 def test_lookup_by_email(self):
     self.logins.add("sofia.fallah@iths.se", "sofia.fallah@iths.se")
     login = self.logins.lookup("sofia.fallah@iths.se")
     self.assertEqual("sofia.fallah@iths.se", login)
+
+
 time.sleep(1)
 conf_email = driver.find_element(By.NAME, "reg_email_confirmation__")
 conf_email.clear()
 conf_email.send_keys("sofia.fallah@iths.se")
-def test_lookup_by_confemail(self):
+
+
+def test_lookup_by_conf_email(self):
     self.logins.add("sofia.fallah@iths.se", "sofia.fallah@iths.se")
     login = self.logins.lookup("sofia.fallah@iths.se")
     self.assertEqual("sofia.fallah@iths.se", login)
+
+
 time.sleep(1)
+
+
 def test_missing_name_raises_error(self):
     with self.assertRaises(KeyError):
         self.logins.lookup("missing")
 
-    def test_empty_logins_is_consistent(self):
-        self.assertTrue(self.logins.is_consistent())
 
 new_pass = driver.find_element(By.NAME, "reg_passwd__")
 new_pass.clear()
--- facebook_test.py
+++ facebook_test.py
@@ -2,15 +2,18 @@ class Login:
     def add(self, name, email):
         self.emails[name] = email
     pass
+
     def __init__(self):
         self.emails = {}
+
     def lookup(self, name):
         return self.emails[name]
-    def is_consistent(self):
-        for name1, email1 in self.emails.items():
-            for name2, email2 in self.emails.items():
-                if email1 == name2:
-                    continue
-                if email1.startswith(email2):
-                    return False
-        return True
+
+ #   def is_consistent(self):
+ #       for name1, email1 in self.emails.items():
+ #           for name2, email2 in self.emails.items():
+ #               if email1 == name2:
+ #                   continue
+ #               if email1.startswith(email2):
+ #                   return False
+ #       return True
